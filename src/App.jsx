4e24b51048112c120b533141dda03fb0f8330575
Alex Velasquez <alex.velasquez08@outlook.com>
2023-06-23 20:40:13 -0300

    NavBar sin estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 
-import Footer from "./Components/Footer";
+import { Route } from "react-router";
 import Navbar from "./Components/Navbar";
-import './App.css';
 
 
-function App() {
+
+/* Paso 1: Creación de rutas */
+/* Importa los componentes pertinentes para envolver la App y crear el enrutado de la página. No importa que aún no tengamos los componentes que irán en cada vista, por el momento podes dejar solamente un div.
+ */function App() {
+
+
   return (
-      <div className="App">
+     <>
           <Navbar/>
-          <Footer/>
-      </div>
+       </>
+      
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useTheme } from '../Context/ThemeContext'
+import {routes }  from './utils/routes' //Aqui deberan importar las rutas
+import { Switch, Route } from 'react-router-dom'
+
+
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
-const Navbar = () => {
 
-  return (
-    <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
-    </nav>
-  )
+const Navbar = () => {
+    return (
+      <header>
+      <a>DH Odonto</a>
+        <nav >
+           
+              
+                    <Link to={routes.home}>Home</Link>
+                
+               
+                    <Link to={routes.favs}>Favs</Link>
+              
+                
+                    <Link to={routes.contact}>Contact</Link>
+               
+           
+        </nav>
+      </header>
+
+
+
+
+
+    )
 }
+
 
 export default Navbar
