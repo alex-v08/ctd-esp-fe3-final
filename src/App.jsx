72c3d059f24e703b315ef5fb04d39e8655985e9c
Alex Velasquez <alex.velasquez08@outlook.com>
2023-06-23 19:40:10 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import './App.css';
+
+
+function App() {
+  return (
+      <div className="App">
+          <Navbar/>
+          <Footer/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useTheme } from '../Context/ThemeContext'
+
+//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+
+const Navbar = () => {
+
+  return (
+    <nav>
+      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
+      <button>Change theme</button>
+    </nav>
+  )
+}
+
+export default Navbar
