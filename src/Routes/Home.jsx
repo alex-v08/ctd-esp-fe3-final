114aa127a33fc08fb8d5fc18497f1d7cdb2db45c
Alex Velasquez <alex.velasquez08@outlook.com>
2023-06-27 16:23:39 -0300

    modifique Home con useReducer

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,43 +1,70 @@
-import React from 'react'
-import { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
-import Card from '../Components/Card'
+import Card from '../Components/Card';
 
+const initialState = {
+  data: [],
+  loading: true,
+  error: null,
+};
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        data: action.payload,
+        loading: false,
+      };
+    case 'FETCH_ERROR':
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
 
 const Home = () => {
-  const [data, setData] = useState([])
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    axios("https://jsonplaceholder.typicode.com/users")
-    .then(res => {
-      
-      setData(res.data)
-      console.log(data)
-    
-    })
-    },[data])
+    axios('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+          dispatch({ type: 'FETCH_SUCCESS', payload: res.data });
+        })
+        .catch(error => {
+          dispatch({ type: 'FETCH_ERROR', payload: error.message });
+        });
+  }, []);
 
-    const cardDent = data.map((dent) =>
-    <div class="col-3">
-    <Card  dent={dent} key={dent.id}/>
-    </div>
-    );
-  
-  return (
+  const { data, loading, error } = state;
 
-  
-
-<div>
-<h1>Home</h1>
-  <div class="container">
-    <div class="row">
-    {cardDent}
-    </div>
-  </div>
-
-</div>
-
-  )
+  if (loading) {
+    return <div>Loading...</div>;
   }
-export default Home
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  const cardDent = data.map(dent => (
+      <div className="col-3" key={dent.id}>
+        <Card dent={dent} />
+      </div>
+  ));
+
+  return (
+      <div>
+        <h1>Home</h1>
+        <div className="container">
+          <div className="row">{cardDent}</div>
+        </div>
+      </div>
+  );
+};
+
+export default Home;
+
